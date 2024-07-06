224a984afd7b277209301b59e97d982711c0531f
emresmsk58 <emre.58.1740@gmail.com>
2024-07-06 20:23:06 +0300

    books tablosunun migrationu oluşturuldu

--- database/migrations/2024_07_06_170446_create_books_table.php
+++ database/migrations/2024_07_06_170446_create_books_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->integer('number_of_pages');
+            $table->date('release_date');
+
+
+
         });
     }
 
